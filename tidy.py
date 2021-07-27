282b093606f6ae85dce748cb84bd447f6f04a114
yangxuan <xuan.yang@zilliz.com>
2021-07-27 17:58:19 +0800

    Add authors and distinguish their emails
    Signed-off-by: yangxuan <xuan.yang@zilliz.com>

--- tidy.py
+++ tidy.py
@@ -3,7 +3,8 @@ import csv
 import time
 
 
-commit_summary = """---------------------------
+commit_summary = """
+---------------------------
 Number ({}):
  - Author: {},
  - Author email: {}
@@ -21,10 +22,11 @@ class TargetRepo:
         self.csvfile = 'eggs.csv' if not csvfile and not isinstance(csvfile, str) else csvfile
         self.repo = git.Repo(path)
         self.commits = []
+        self._authors = {}
         self.author2email = {}
         self._commits = []
 
-    def init_commits(self, branch: str, paths=None):
+    def init_commits(self, branch: str, write_csv=False, paths=None):
         #  time.asctime(time.localtime(c.authored_date)
         self.commits = [(c.author.name,
                          c.author.email,
@@ -38,9 +40,19 @@ class TargetRepo:
 
         self.commits.reverse()
 
-        with open(self.csvfile, 'w', newline='') as csvfile:
-            commitwriter = csv.writer(csvfile)
-            commitwriter.writerows(self.commits)
+        for c in self.repo.iter_commits(branch, paths):
+            emails = [] if not self._authors.get(c.author.name, None) else self._authors.get(c.author.name)
+            emails.append(c.author.email)
+            self._authors[c.author.name] = list(set(emails))
+
+        if write_csv:
+            with open(self.csvfile, 'w', newline='') as csvfile:
+                commitwriter = csv.writer(csvfile)
+                commitwriter.writerows(self.commits)
+
+    @property
+    def authors(self):
+        return self._authors
 
     def read_commits(self):
         with open(self.csvfile, newline='') as csvfile:
@@ -62,11 +74,20 @@ class TargetRepo:
         return res
 
 
+def test_authors(repo: TargetRepo):
+    print("== Test Authors ==")
+    repo.init_commits("main", False)
+
+    print(repo.authors)
+
+
 if __name__ == "__main__":
     REPO = "/home/yangxuan/Github/pymilvus-orm"
     pymilvus_orm = TargetRepo(REPO)
 
-    #  pymilvus_orm.init_commits("main", ["pymilvus_orm", "tests", "docs"])
+    #  pymilvus_orm.init_commits("main", True, ["pymilvus_orm", "tests", "docs"])
     #  print(pymilvus_orm)
-    pymilvus_orm.read_commits()
-    print(pymilvus_orm)
+    #  pymilvus_orm.read_commits()
+    #  print(pymilvus_orm)
+
+    test_authors(pymilvus_orm)
